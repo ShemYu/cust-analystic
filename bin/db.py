8f3e046578ee62463247285096ec344c6ce8b2d2
Shem <shem.yu@tripsaas.com>
2020-10-26 17:12:25 +0800

    [Chg] Set config sections

--- bin/db.py
+++ bin/db.py
@@ -12,21 +12,29 @@ class db_connector(object):
         self.config = configparser.ConfigParser()
         self.config.read('doc/db_config.ini')
         self.default = self.config['DEFAULT']
-        self.db = MySQLdb.connect(
-            host=self.default['host'], user=self.default['user'], passwd=self.default['passwd'], db=self.default['db'], charset=self.default['charset'])
-        self.cursor = self.db.cursor()
+        try:
+            self.db = MySQLdb.connect(
+                host=self.default['host'], user=self.default['user'], passwd=self.default['passwd'], db=self.default['db'], charset=self.default['charset'])
+            self.cursor = self.db.cursor()
+        except:
+            self.db = ''
+            self.cursor = ''
 
     def query(self,sql,com=False):
         """
         input : sql
         output : tuple data
         """
-        self.cursor.execute(sql)
-        results = self.cursor.fetchall()
-        if com:
-            self.db.commit()
-        # print(type(results))
-        return results
+        if self.db and self.cursor:
+            self.cursor.execute(sql)
+            results = self.cursor.fetchall()
+            if com:
+                self.db.commit()
+            # print(type(results))
+            return results
+        else:
+            print('Please set_config with section_name first!')
+            return ''
         
     def idu(self,sql):
         self.cursor.execute(sql)
@@ -34,3 +42,12 @@ class db_connector(object):
 
     def close(self):
         self.db.close()
+
+    def set_config(self, section_name=''):
+        if section_name and section_name in self.config:
+            self.default = self.config[section_name]
+            self.db = MySQLdb.connect(
+                host=self.default['host'], user=self.default['user'], passwd=self.default['passwd'], db=self.default['db'], charset=self.default['charset'])
+            self.cursor = self.db.cursor()
+        else:
+            print('Please check the section name!')
